531aba2484119088dcdbc2b5cc4390c70dcd3418
SkyExit <60986116+SkyExit@users.noreply.github.com>
2021-01-18 15:36:46 +0100

    Update WindowTest.java

--- src/WindowTest.java
+++ src/WindowTest.java
@@ -6,6 +6,8 @@ import java.awt.event.*;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 
 public class WindowTest {
@@ -15,26 +17,20 @@ public class WindowTest {
         FileReader reader = new FileReader(configFile);
         Properties props = new Properties();
         props.load(reader);
-
         DiscordRPC lib = DiscordRPC.INSTANCE;
         DiscordRichPresence presence = new DiscordRichPresence();
         String applicationId = props.getProperty("appID");
-        String steamId       = args.length < 2 ? "" : args[1];
-
+        String steamId = (args.length < 2) ? "" : args[1];
         DiscordEventHandlers handlers = new DiscordEventHandlers();
-        handlers.ready = (user) -> System.out.println("Ready!");
-
+        handlers.ready = (user -> System.out.println("Ready!"));
         lib.Discord_Initialize(applicationId, handlers, true, steamId);
-
-        presence.startTimestamp = 1580545800;
-        presence.details   = "details";
-        presence.state     = "state";
-
-        presence.largeImageKey = "largeImageKey";
-        presence.largeImageText = "largeImageText";
-
-        presence.smallImageKey = "smallImageKey";
-        presence.smallImageText = "smallImageText";
+        presence.startTimestamp = 1601271600;
+        presence.details = "Fach";
+        presence.state = "In BBB";
+        presence.largeImageKey = "wg3";
+        presence.largeImageText = "Wilhelms-Gymnasium";
+        presence.smallImageKey = "sky2";
+        presence.smallImageText = "SkyExit";
         lib.Discord_UpdatePresence(presence);
 
         Thread t = new Thread(() -> {
@@ -91,6 +87,15 @@ public class WindowTest {
         sImageLabelT.setHorizontalAlignment(SwingConstants.RIGHT);
         JTextField sImageText = new JTextField(presence.smallImageText);
 
+        JLabel sTimeStampL = new JLabel("Start");
+        sTimeStampL.setHorizontalAlignment(SwingConstants.RIGHT);
+        JTextField sTimeStampT = new JTextField("07:40");
+
+        JLabel fillerL = new JLabel("Placeholder");
+        fillerL.setHorizontalAlignment(SwingConstants.RIGHT);
+        JTextField fillerT = new JTextField("Placeholder");
+
+
 
         JButton submit = new JButton("Update RPC");
         submit.addActionListener(e -> {
@@ -100,6 +105,17 @@ public class WindowTest {
             presence.largeImageText = lImageText.getText();
             presence.smallImageKey = sImageKey.getText();
             presence.smallImageText = sImageText.getText();
+
+            try {
+                String myDate = "2021/01/18 " + sTimeStampT.getText() + ":00";
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                Date date = sdf.parse(myDate);
+                long millis = date.getTime();
+
+                presence.startTimestamp = millis;
+            } catch (Exception ignored) {
+                presence.startTimestamp = Integer.parseInt(sTimeStampT.getText());
+            }
 
             lib.Discord_UpdatePresence(presence);
 
@@ -119,9 +135,14 @@ public class WindowTest {
         top.add(sImageLabelT);
         top.add(sImageText);
 
+        top.add(sTimeStampL);
+        top.add(sTimeStampT);
+        top.add(fillerL);
+        top.add(fillerT);
+
         bottom.add(new JPanel());
         bottom.add(submit);
-        bottom.add(new JPanel()); // dummy components to center the update button
+        bottom.add(new JPanel());
 
         bottom.add(appKey);
 
